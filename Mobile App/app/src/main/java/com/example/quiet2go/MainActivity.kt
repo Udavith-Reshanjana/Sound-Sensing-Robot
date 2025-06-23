2ae269e771862d42c7affaaad99b7806f288b48a
Udavith Reshanjana <146712079+Udavith-Reshanjana@users.noreply.github.com>
2025-06-23 23:39:59 +0530

    splash screen updated

--- Mobile App/app/src/main/java/com/example/quiet2go/MainActivity.kt
+++ Mobile App/app/src/main/java/com/example/quiet2go/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.quiet2go
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
     }
 }

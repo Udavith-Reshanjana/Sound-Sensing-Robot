29cd11c7f2076b8b0a23063f1e3f7114a3a8a31e
Udavith Reshanjana <146712079+Udavith-Reshanjana@users.noreply.github.com>
2025-06-26 17:00:27 +0530

    Adjust sensor error
    Modified the calculation of the rounded average to add a constant value of 55, representing the sensor's measurement error, when the adjusted average is not zero.

--- Mobile App/app/src/main/java/com/example/quiet2go/HomeActivity.kt
+++ Mobile App/app/src/main/java/com/example/quiet2go/HomeActivity.kt
@@ -133,7 +133,12 @@ class HomeActivity : AppCompatActivity() {
                                     }
 
                                     val adjustedAvg = if (avg >= 0) avg * 0.01 else 0.0
-                                    val roundedAvg = DecimalFormat("#.##").format(adjustedAvg)
+                                    val roundedAvg = if (adjustedAvg != 0.0) {
+                                        val correctedAvg = adjustedAvg + 55 // Add the measurement error
+                                        DecimalFormat("#.##").format(correctedAvg) // Format the corrected value
+                                    } else {
+                                        "0.0"
+                                    }
 
                                     Log.d("Parsed Data", "Avg: $roundedAvg, Level: $level") // Log parsed values
 
